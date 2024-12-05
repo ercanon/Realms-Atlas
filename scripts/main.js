ab56f8d32cfd095ed4dd27ff3e40df0625b3ab55
ercanon <61042236+ercanon@users.noreply.github.com>
2024-12-05 17:26:35 +0100

    Personalized Interactive Map

--- scripts/main.js
+++ scripts/main.js
@@ -12,119 +12,142 @@ document.getElementById('shareBtn').addEventListener('click', async () =>
             navigator.clipboard.writeText(window.location.href);
     }
     catch (error) {
-        console.error("Error al compartir:", error);
-        alert("No se pudo compartir el enlace.");
+        return showError("No se pudo compartir el enlace.", error);
     }
 });
 
 document.getElementById('uploadImg').addEventListener('change', async function ()
 {
-    loadImg(this.files[0]);
-})
+    const file = this.files[0];
+    if (!file)
+        return showError('No file selected.');
 
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload  = (event) => loadImg(event.target.result);
+    reader.onerror = (event) => { return showError('An error occurred while reading the file.', event.target.error); }
+})
 document.getElementById('imgUrl').addEventListener('change', async function ()
 {
-    const response = await fetch(this.value.trim());
-    if (!response.ok)
-        alert(`Failed to fetch image from URL: ${response.statusText}`);
-    else
-        loadImg(await response.blob());
+    const url = this.value.trim();
+    if (!url)
+        return showError('Please, provide a valid image URL.');
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok)
+            return showError(`Failed to fetch image from URL: ${response.statusText}`);
+
+        loadImg(response.url);
+    }
+    catch (error) {
+        return showError('An error occurred while fetching the image.', error);
+    }
 })
 
+
+
+/*>--------------- { ImageHandeler } ---------------<*/
 //const dataBase = new DataBase('TileDataBase');
 const mapRender = document.getElementById('mapRender');
-const fileInput = document.getElementById('fileInput');
-async function loadImg(inputFile)
+const inputRender = document.getElementById('fileInput');
+function loadImg(inputsrc)
 {
-    if (!inputFile)
-        return alert("Please, upload an image or submit a valid url.");
+    if (!inputsrc)
+        return showError("Please, upload an image or submit a valid url.");
 
     mapRender.innerHTML = "";
-
-    const zoomSettings = { lvl: 4, res: 256};
-    try
-    {
-        await imgTiler(inputFile, zoomSettings);
+    const zoomSettings = { lvl: 2, res: 256};
+    try {
+        const img = new Image();
+        img.src = inputsrc;
+        img.onload = () => loadLayer(img, zoomSettings);
 
         mapRender.style.display = 'block';
-        fileInput.style.display = 'none';
+        inputRender.style.display = 'none';
     }
-    catch (error)
-    {
-        console.error('Error processing image:', error);
+    catch (error) {
+        return showError('Error loading the image.', error);
     }
 }
 
-async function imgTiler(inputFile, zoomSettings)
+function showError(message, error = null) {
+    console.error(message, error);
+    alert(message);
+}
+
+
+
+/*>--------------- { MapHandeler } ---------------<*/
+const CanvasLayer = L.GridLayer.extend({
+    options: {
+        // The minimum zoom level down to which this layer will be displayed (inclusive).
+        minZoom: 0,
+
+        // The maximum zoom level up to which this layer will be displayed (inclusive).
+        maxZoom: 18,
+
+        // The zoom number used in tile URLs will be offset with this value.
+        zoomOffset: 0,
+
+        // If `true`, inverses Y axis numbering for tiles (turn this on for [TMS](https://en.wikipedia.org/wiki/Tile_Map_Service) services).
+        tms: false,
+
+        // If set to true, the zoom number used in tile URLs will be reversed (`maxZoom - zoom` instead of `zoom`)
+        zoomReverse: false,
+
+        //Image to be tiled and rendered in the map
+        img: null
+    },
+    createTile: function (coords, done) {
+        const { x, y, z } = coords;
+        const resTile = this.options.tileSize * Math.pow(2, this.options.maxNativeZoom - z);
+
+        const tileCanvas = L.DomUtil.create('canvas', 'leaflet-tile');
+        tileCanvas.width = tileCanvas.height = resTile;
+
+        this._getTile(x, y, resTile).then(tile => { 
+            if (tile) 
+                tileCanvas.getContext('2d').drawImage(tile, 0, 0);
+            done(null, tileCanvas);
+        }).catch(error => {
+            showError("Error generating tiles.", error);
+            done(error, null);
+        });
+        return tileCanvas;
+    },
+    _getTile: async function (x, y, resTile)
+    {
+        const img = this.options.img;
+
+        const tileX = x * resTile;
+        const tileY = y * resTile;
+
+        if (tileX >= img.width || tileY >= img.height || tileX < 0 || tileY < 0)
+            return null;
+
+        return await createImageBitmap(
+            img,
+            tileX,
+            tileY,
+            resTile,
+            resTile
+        );
+    }
+});
+function loadLayer(img, zoomSettings)
 {
-    const tileCache = {};
-    let resPow = zoomSettings.res/2;
-    const imgBitmap = await createImageBitmap(inputFile);
-
-    for (let z = 0; z <= zoomSettings.lvl; z++)
-    {
-        resPow *= 2;
-        const paddedImg = addPadding(imgBitmap, resPow);
-
-        const rows = Math.ceil(paddedImg.width  / resPow);
-        const cols = Math.ceil(paddedImg.height / resPow);
-
-        tileCache[z] = {};
-        for (let x = 0; x < rows; x++) {
-            for (let y = 0; y < cols; y++)
-            {
-                const tileCanvas  = document.createElement('canvas');
-                tileCanvas.width  = resPow;
-                tileCanvas.height = resPow;
-                
-                tileCanvas.getContext('2d').drawImage(
-                    paddedImg,
-                    x * resPow, y * resPow, resPow, resPow,
-                    0, 0, resPow, resPow
-                );
-                
-                tileCache[z][`${x}_${y}`] = tileCanvas;
-            }
-        }
-    }
-
-    loadMap(tileCache, zoomSettings, [-imgBitmap.height, imgBitmap.width]);
-}
-function addPadding(imgBitmap, dimension) {
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-
-    canvas.width = imgBitmap.width + getPadding(imgBitmap.width, dimension);
-    canvas.height = imgBitmap.height + getPadding(imgBitmap.height, dimension);
-
-    ctx.fillStyle = 'rgba(0,0,0,0)'; // Transparent background
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(imgBitmap, 0, 0);
-
-    return canvas;
-}
-function getPadding(dimension, cropDimension) {
-    if (dimension % cropDimension === 0) return 0;
-    return cropDimension - (dimension % cropDimension);
-}
-
-function loadMap(tileCache, zoomSettings, imgSize) {
     const map = L.map(mapRender, {
         crs: L.CRS.Simple,
-        maxBounds: [[0, 0], imgSize],
         maxBoundsViscosity: 1.0,
-        noWrap: true
-    }).setView([imgSize[0] / 2, imgSize[1] / 2], 0);
+        noWrap: true,
+    }).setView([0, 0], 0);
 
-    const CustomGridLayer = L.GridLayer.extend({
-        createTile: function (coords) {
-            const { x, y, z } = coords;
-            return tileCache[z][`${x}_${y}`] ?? document.createElement('canvas');
-        }
-    });
-
-    new CustomGridLayer({
-        attribution: 'Image Tiler Based on github.com/Simperfy/img2-Leaftlet-Tile',
-        maxZoom: zoomSettings.lvl
+    new CanvasLayer({
+        attribution: 'Image Tiler Based on <a href="https://github.com/Simperfy/img2-Leaftlet-Tile" target="_blank">Simperfy/img2-Leaftlet-Tile</a>',
+        maxNativeZoom: zoomSettings.lvl,
+        bounds: [[0, 0], [-img.height, img.width]],
+        img: img
     }).addTo(map);
 }
